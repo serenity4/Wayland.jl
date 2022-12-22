a9bc07ac28742c0c303fffbb0053b6aa8a874964
Documenter.jl <documenter@juliadocs.github.io>
2022-12-22 16:40:40 +0000

    build based on b339c81

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"reference/api/#API-Reference","page":"API","title":"API Reference","text":"","category":"section"},{"location":"#Wayland.jl","page":"Home","title":"Wayland.jl","text":"","category":"section"},{"location":"#Status","page":"Home","title":"Status","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This package is currently in development. The source code and public API may change at any moment. Use at your own risk.","category":"page"}]
+}

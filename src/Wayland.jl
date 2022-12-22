99000a2ef081db8bed694af1ef3f71483199f953
serenity4 <cedric.bel@hotmail.fr>
2022-12-22 17:41:09 +0100

    Second commit

--- src/Wayland.jl
+++ src/Wayland.jl
@@ -1,3 +1,5 @@
 module Wayland
 
+using WindowAbstractions
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,8 @@
 using Wayland, Test
-using Wayland: Expression, isexpr, postwalk, simplify!, simplified, isweighted, getcomponent, blade, factor, weighted, scalar, antiscalar, kvector, multivector, antigrade, antireverse, exterior_product, ⟑, ∧, Head, FACTOR, BLADE, KVECTOR, MULTIVECTOR, ADDITION, SUBTRACTION, NEGATION, REVERSE, ANTIREVERSE, LEFT_COMPLEMENT, RIGHT_COMPLEMENT, GEOMETRIC_PRODUCT, EXTERIOR_PRODUCT, INTERIOR_PRODUCT, COMMUTATOR_PRODUCT, INVERSE, EXPONENTIAL
 
 ENV["JULIA_DEBUG"] = "Wayland"
 ENV["JULIA_DEBUG"] = ""
 
 @testset "Wayland.jl" begin
-    include("signatures.jl")
-    include("expressions.jl")
-    include("passes.jl")
-    include("types.jl")
-    include("macro.jl")
-    include("operators.jl")
-    include("examples.jl")
-    include("doctests.jl")
+    @test true
 end;
